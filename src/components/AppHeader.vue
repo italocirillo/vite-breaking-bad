5c0c2c2e6cc818802551660d86059c0ffef6979c
italocirillo <italocirillo@gmail.com>
2023-04-17 22:18:34 +0200

    HEADER COMPLETO

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <header>
+        <img src="../assets/img/logo.svg" alt="logo">
+        <h1>
+            Yu-Gi-Oh Api
+        </h1>
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    display: flex;
+    padding: 1em;
+
+    img {
+        width: 10%;
+        padding: 0em 1em;
+    }
+
+    h1 {
+        font-weight: 900;
+        padding: 0em 0.5em;
+    }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "AppMain",
+}
+</script>
+
+<template>
+    <h2>Main</h2>
+</template>
+
+<style lang="scss">
+h2 {
+    border: 5px solid gray;
+}
+</style>
