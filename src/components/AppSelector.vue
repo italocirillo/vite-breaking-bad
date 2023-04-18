0830d7d95c3235865ab753b218af0443176075ea
italocirillo <italocirillo@gmail.com>
2023-04-18 21:52:58 +0200

    ESERCIZIO COMPLETO

--- src/components/AppSelector.vue
+++ src/components/AppSelector.vue
@@ -14,9 +14,26 @@ export default {
 </script>
 
 <template>
-    <select class="form-select" name="tipo" id="tipo" v-model="store.tipoSelezionato" @change="$emit('filter')">
-        <option value="">Tutti</option>
-        <option :value="tipo" v-for="tipo in tipi">{{ tipo }}</option>
-    </select>
+    <div class="sezione-selettore">
+        <div class="container">
+            <select class="form-select" name="tipo" id="tipo" v-model="store.tipoSelezionato" @change="$emit('filter')">
+                <option value="">Tutti</option>
+                <option :value="tipo" v-for="tipo in tipi">{{ tipo }}</option>
+            </select>
+        </div>
+    </div>
 </template>
+
+<style lang="scss">
+@use "../styles/partials/variables" as*;
+
+.sezione-selettore {
+    background-color: $background-color;
+    padding: 2em 2em 0em 0em;
+
+    .form-select {
+        width: 40%;
+    }
+}
+</style>
 
